73f23dc075d3637ef41572a452ac6fc05d0d8a91
David Lewis <david.lewis@wayveapp.com>
2017-08-14 22:05:54 +0100

    Grid filtering
    This commit introduces the grid filter and related tests

--- decision-tech-react-seed/src/Components/grid-component/grid-component.jsx
+++ decision-tech-react-seed/src/Components/grid-component/grid-component.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 export class Grid extends React.Component {
   render() {
-    const rows = this.props.gridData.map((rowData, rowIndex) => {
+    const filteredRows = this.props.gridData.filter(rowData =>
+      this.props.filters.reduce((acc, filter) => {
+        return acc && filter(rowData);
+      }, true)
+    );
+    const rows = filteredRows.map((rowData, rowIndex) => {
       const cells = this.props.definition.map((d, cellindex) => {
         return <div key={`gridRow${rowIndex}cell${cellindex}`} className="gridCell">{d.render(rowData)}</div>
       });
       return cells;
     });
-    const header = this.props.definition.map((d) => <div className="gridHeading">d.title</div>)
+    const header = this.props.definition.map((d) => <div key={d.title} className="gridHeading">d.title</div>)
     return (<div className="dataGrid">
-      <div className="gridHeader">{header}</div>
+      <div key="gridHeader" className="gridHeader">{header}</div>
       {rows}
     </div>);
   }
